7dc8ca706483d6fb1ca4f4144bc56142d79bb061
Augustin Bralley <augustin@scribd.com>
2020-05-19 11:31:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+##Prereqs
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+##Locally
+	pip3 install -r requirements.txt
+	python3 app.py
+
+
+##Container
+	docker build -t summarizer .
+	docker run -p 80:80 summarizer .
+
+##Example usage
+
+curl -X POST   \
+ ec2-3-94-112-129.compute-1.amazonaws.com/small  \
+-H 'Content-Type: application/json' \
+-d '{"text":"The good news is that, after digging into this, there is a lot of low hanging fruit (and regardless of the current circumstances, it’s good company hygiene for us to look back at expenses and consider if there are things we can eliminate). Let me start by saying, this is a work in progress. Changing infrastructure expenses, for example, isn’t an overnight project. We’ve identified about $6M in expenses that we think we can cut and many of you have already been looped into investigating these opportunities.", "token":"this secret should not be hardcoded like this"}'
+
+##How tos:
+
+https://towardsdatascience.com/simple-way-to-deploy-machine-learning-models-to-cloud-fd58b771fdcf
+
+https://medium.com/runwayml/creating-a-custom-gpt-2-slack-bot-with-runwaymls-hosted-models-c639fe135379
+
---
+++ app.py
@@ -0,0 +1,91 @@
+# Serve T5 model as a flask application
+
+import torch
+import json
+import time
+from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
+from flask import Flask, request
+
+secret_token = 'this secret should not be hardcoded like this'
+model_large = None
+tokenizer_large = None
+model_small = None
+tokenizer_small = None
+device = None
+app = Flask(__name__)
+
+def load_model():
+		global model_large
+		global tokenizer_large
+		global model_small
+		global tokenizer_small
+		global device
+		# Download and save the models:
+		# Large
+		# model = T5ForConditionalGeneration.from_pretrained('t5-large')
+		# model.save_pretrained('./large')
+		# tokenizer.save_pretrained('./large')
+		# Small
+		# model = T5ForConditionalGeneration.from_pretrained('t5-small')
+		# model.save_pretrained('./small')
+		# tokenizer.save_pretrained('./small')
+		print ("loading models...")
+		model_large = T5ForConditionalGeneration.from_pretrained('./large')
+		tokenizer_large = T5Tokenizer.from_pretrained('./large')
+		model_small = T5ForConditionalGeneration.from_pretrained('./small')
+		tokenizer_small = T5Tokenizer.from_pretrained('./small')
+		device = torch.device('cpu')
+		print ("models loaded...")
+
+def getTextFromRequest(request):
+	data = request.get_json()
+	preprocess_text = data['text'].strip().replace("\n","")
+	print ("original text preprocessed: \n", preprocess_text)
+	return "summarize: "+ preprocess_text
+
+def getSummary(tokenizer, model, prepared_text):
+	tokenized_text = tokenizer.encode(prepared_text, return_tensors="pt").to(device)
+	summary_ids = model.generate(tokenized_text,
+																			num_beams=4,
+																			no_repeat_ngram_size=2,
+																			min_length=30,
+																			max_length=100,
+																			early_stopping=True)
+	output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+	print ("\n\nSummarized text: \n", output)
+	return output
+
+def hasToken(request):
+	data = request.get_json()
+	if data['token'] == secret_token: 
+		return True
+	else:
+		return False
+
+@app.route('/')
+def home_endpoint():
+    return 'Nothing to see here... move along.'
+
+@app.route('/large', methods=['POST'])
+def get_large_summary():
+		if hasToken(request) == False: return 'permission denied'
+		start_time = time.time()
+		if request.method == 'POST':
+				t5_prepared_Text = getTextFromRequest(request)
+				output = getSummary(tokenizer_large, model_large, t5_prepared_Text)
+		print("--- %s seconds ---" % (time.time() - start_time))
+		return str(output)
+
+@app.route('/small', methods=['POST'])
+def get_small_summary():
+		if hasToken(request) == False: return 'permission denied'
+		start_time = time.time()
+		if request.method == 'POST':
+				t5_prepared_Text = getTextFromRequest(request)
+				output = getSummary(tokenizer_small, model_small, t5_prepared_Text)
+		print("--- %s seconds ---" % (time.time() - start_time))
+		return str(output)
+
+if __name__ == '__main__':
+    load_model()  # load model at the beginning once only
+    app.run(host='0.0.0.0', port=80)
